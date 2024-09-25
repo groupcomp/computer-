f0cfe1861a4837f2845d0db8709c303f5120fc59
groupcomp <srushtishewale2024.comp@mmcoe.edu.in>
2024-09-25 12:25:44 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int a,b;
+   char s;
+   printf("Enter any two numbers")
+   scanf("%d","%c"&a,&b)
+   
+   switch(s)
+
+   return 0;
+}
